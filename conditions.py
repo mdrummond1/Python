0d1596fb9cd439f5e372bc7b2231525b48a092fe
Matthew Drummond <mattatatter@gmail.com>
2019-11-20 10:32:40 -0600

    added comparison operators

--- conditions.py
+++ conditions.py
@@ -1,3 +1,18 @@
+"""comparisons"""
+def max(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+def sameStr(string1, string2):
+    if string1 == string2:
+        return True
+    else:
+        return False
+
 blue = False
 is_tall = True
 
@@ -8,4 +23,7 @@ elif is_tall and not(blue):
 else:
     print("It's not blue, or you're a shorypants")
 
+print("max: " + str(max(3, 4, 5)))
+
+print("same String: " + str(sameStr("green", "food")))
 
