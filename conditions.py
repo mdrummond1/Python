0f5660e65bdf24a06561994960b488f5b5ba445b
Matthew Drummond <mattatatter@gmail.com>
2019-11-20 10:25:34 -0600

    added elif

--- conditions.py
+++ conditions.py
@@ -1,8 +1,10 @@
-blue = True
-is_tall = False
+blue = False
+is_tall = True
 
 if blue and is_tall:
     print("it's blue! You tall, man")
+elif is_tall and not(blue):
+    print ("You're a short non-blue person")
 else:
     print("It's not blue, or you're a shorypants")
 
