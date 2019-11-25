580f7070b65f3795eab7cf6d43eb6291b0db9a62
Matthew Drummond <mattatatter@gmail.com>
2019-11-25 09:07:00 -0600

    added base case to fact.

--- fact.py
+++ fact.py
@@ -1,4 +1,9 @@
 def fact(num):
+    if num == 0:
+        return 1
     return num * fact(num - 1)
 
-print("fact 4: " + str(fact(4)))
+get = int(input("enter max factorial to find: ")) + 1
+
+for i in range(get):
+    print("fact " + str(i) + ": " + str(fact(i)))
