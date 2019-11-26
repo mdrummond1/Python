b28b92261f9b8dcac3c901c161ed056f1380abf3
Test <mattatatter@gmail.com>
2019-11-26 13:01:50 -0600

    changes to factorial

--- fact.py
+++ fact.py
@@ -1,4 +1,10 @@
 def fact(num):
-    return num * fact(num - 1)
+    if num == 0:
+        return 1
+    else:
+        return num * fact(num - 1)
 
-print("fact 4: " + str(fact(4)))
+num = int(input("How high do you want to go?")) + 1
+
+for i in range(num):
+    print("fact " + str(i) + ": " + str(fact(i)))
