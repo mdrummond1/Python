50928afb052990b585013987a47d02af57c7c897
Test <mattatatter@gmail.com>
2019-12-01 15:27:29 -0600

    a few files to work with inheritance

---
+++ ChineseChef.py
@@ -0,0 +1,8 @@
+from Chef import Chef
+class ChineseChef(Chef):
+    
+    def make_special_dish(self):
+        print("The chef made orange chicken")
+
+    def make_fried_rice(self):
+        print("The chef made fried rice")
