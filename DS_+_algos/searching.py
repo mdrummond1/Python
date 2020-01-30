4755061d37e220dbc77625400354eacf3c3a7666
Test <mattatatter@gmail.com>
2020-01-30 16:20:56 -0600

    added linear search algorithm to searching.py

---
+++ DS_+_algos/searching.py
@@ -0,0 +1,9 @@
+def linearSearch(array, x):
+    for i in range(0, len(array)):
+        if array[i] == x:
+            return i
+            print("found!")
+
+array = [2, 5, 4, 7, 9, 8]
+
+print(linearSearch(array, 10))
